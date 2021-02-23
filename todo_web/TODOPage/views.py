97602ec704583b53ae83c528b84e2c4ac2191b73
Sivaprasad. G <Sprasad>
2021-02-23 14:11:35 +0530

    addtodoapp

--- todo_web/TODOPage/views.py
+++ todo_web/TODOPage/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpRequest
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
+
+
 # Create your views here.
+def index(request):
+    return HttpResponse("Hello Siva")
